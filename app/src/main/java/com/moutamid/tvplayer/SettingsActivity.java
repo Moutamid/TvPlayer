625e5615216674f77cdd1309a5c9d321830275b7
suleman81 <sunnybajwa904@gmail.com>
2023-01-07 20:14:04 +0500

    commit

--- app/src/main/java/com/moutamid/tvplayer/SettingsActivity.java
+++ app/src/main/java/com/moutamid/tvplayer/SettingsActivity.java
@@ -1,14 +1,23 @@
 package com.moutamid.tvplayer;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+import com.moutamid.tvplayer.databinding.ActivitySettingsBinding;
 
+public class SettingsActivity extends AppCompatActivity {
+    ActivitySettingsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        /*ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);*/
+
     }
 }
