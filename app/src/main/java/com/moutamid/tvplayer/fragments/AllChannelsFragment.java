67f7aa283a71685a71e07b714312009adeca0357
suleman81 <sunnybajwa904@gmail.com>
2023-01-07 19:13:36 +0500

    commit

--- app/src/main/java/com/moutamid/tvplayer/fragments/AllChannelsFragment.java
+++ app/src/main/java/com/moutamid/tvplayer/fragments/AllChannelsFragment.java
@@ -1,22 +1,43 @@
 package com.moutamid.tvplayer.fragments;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.material.tabs.TabLayout;
 import com.moutamid.tvplayer.R;
+import com.moutamid.tvplayer.adapters.ChannelsAdapter;
 import com.moutamid.tvplayer.databinding.FragmentAllChannelsBinding;
+import com.moutamid.tvplayer.models.ChannelsModel;
+import com.moutamid.tvplayer.models.StreamLinksModel;
 import com.moutamid.tvplayer.models.TabsModel;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Locale;
 
 public class AllChannelsFragment extends Fragment {
 
@@ -40,8 +61,25 @@ public class AllChannelsFragment extends Fragment {
 
         addTabs();
 
-        viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+        viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager(), binding.tablayout.getTabCount());
         binding.viewpager.setAdapter(viewPagerAdapter);
+
+        binding.tablayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                binding.viewpager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         binding.tablayout.setupWithViewPager(binding.viewpager);
 
@@ -49,41 +87,92 @@ public class AllChannelsFragment extends Fragment {
     }
 
     private void addTabs() {
-        tabsModel = new TabsModel("Sports", new SportsFragment());
-        tabs.add(tabsModel);
+        new Thread(() -> {
+            URL google = null;
+            try {
+                google = new URL("http://95.217.210.178/api/v1/channel/");
+            } catch (final MalformedURLException e) {
+                e.printStackTrace();
+            }
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(google != null ? google.openStream() : null));
+            } catch (final IOException e) {
+                Log.d("TAG", "compress: ERROR: " + e.toString());
+                e.printStackTrace();
+            }
+            String input = null;
+            StringBuffer stringBuffer = new StringBuffer();
+            while (true) {
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        if ((input = in != null ? in.readLine() : null) == null) break;
+                    }
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+                stringBuffer.append(input);
+            }
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+            String htmlData = stringBuffer.toString();
 
-        tabsModel = new TabsModel("News", new NewsFragment());
-        tabs.add(tabsModel);
+            Log.d("TAG", "compress: " + htmlData);
 
-        tabsModel = new TabsModel("Events", new EventsFragment());
-        tabs.add(tabsModel);
+            requireActivity().runOnUiThread(() -> {
+                try {
+                    JSONObject jsonObject = new JSONObject(htmlData);
+                    JSONObject data = jsonObject.getJSONObject("data");
+                    for (String s : iterate(data.keys())) {
+                        binding.tablayout.addTab(binding.tablayout.newTab().setText(s.toUpperCase(Locale.ROOT)));
+                    }
 
-        tabsModel = new TabsModel("Favourites", new FavouritesFragment());
-        tabs.add(tabsModel);
+                } catch (JSONException error) {
+                    Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }).start();
     }
 
-    public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        int mNumOfTabs;
+        Fragment fragment = null;
 
-        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int mNumOfTabs) {
             super(fm);
+            this.mNumOfTabs = mNumOfTabs;
         }
 
         @NonNull
         @Override
         public Fragment getItem(int position) {
-            Fragment fragment = tabs.get(position).getFragment();
+            for (int i = 0; i < mNumOfTabs ; i++) {
+                if (i == position) {
+                    fragment = new CommonFragment(getPageTitle(i).toString().toUpperCase(Locale.ROOT));
+                    break;
+                }
+            }
             return fragment;
         }
 
         @Override
         public int getCount() {
-            return tabs.size();
+            return mNumOfTabs;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            String title = tabs.get(position).getTitle();
-            return title;
+            return super.getPageTitle(position);
         }
+
+    }
+
+    private <T> Iterable<T> iterate(final Iterator<T> i) {
+        return () -> i;
     }
 }
--- app/src/main/java/com/moutamid/tvplayer/fragments/CommonFragment.java
+++ app/src/main/java/com/moutamid/tvplayer/fragments/CommonFragment.java
@@ -1,5 +1,6 @@
 package com.moutamid.tvplayer.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,16 +10,31 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.moutamid.tvplayer.R;
+import com.moutamid.tvplayer.adapters.ChannelsAdapter;
+import com.moutamid.tvplayer.models.ChannelsModel;
+import com.moutamid.tvplayer.models.StreamLinksModel;
+
+import java.util.ArrayList;
 
 public class CommonFragment extends Fragment {
+
+    String title;
+    Context context;
+    ArrayList<ChannelsModel> channelsList;
+    ArrayList<StreamLinksModel> streamLinks;
+    ChannelsAdapter adapter;
 
     public CommonFragment() {
         // Required empty public constructor
     }
 
+    public CommonFragment(String title) {
+        this.title = title;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_common, container, false);
     }
 }
