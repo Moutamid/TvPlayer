965525e270cbe8ce2a90dba5af25145b70444545
suleman81 <sunnybajwa904@gmail.com>
2023-01-11 22:09:06 +0500

    commit

--- app/src/main/java/com/moutamid/tvplayer/MetaRequest.java
+++ app/src/main/java/com/moutamid/tvplayer/MetaRequest.java
@@ -1,17 +1,22 @@
 package com.moutamid.tvplayer;
 
+import android.util.Log;
+
 import androidx.annotation.Nullable;
 
+import com.android.volley.Header;
 import com.android.volley.NetworkResponse;
 import com.android.volley.ParseError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.fxn.stash.Stash;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class MetaRequest  extends JsonObjectRequest {
     public MetaRequest(String url, Response.Listener<JSONObject> listener, @Nullable Response.ErrorListener errorListener) {
@@ -25,15 +30,20 @@ public class MetaRequest  extends JsonObjectRequest {
     @Override
     protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
         try {
-            String jsonString = new String(response.data,
-                    HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
-            JSONObject jsonResponse = new JSONObject(jsonString);
+            Log.d("htmlTAG", "data " + response.data);
+            String jsonString = new String(response.data, HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
+            JSONObject jsonResponse = new JSONObject("{" + jsonString + "}");
+            List<Header> headers = response.allHeaders;
+            Stash.put("headers", headers);
+            Stash.put("SeassionHeader", response.headers.get("Session"));
             jsonResponse.put("headers", new JSONObject(response.headers));
             return Response.success(jsonResponse,
                     HttpHeaderParser.parseCacheHeaders(response));
         } catch (UnsupportedEncodingException e) {
+            Log.d("htmlTAG", "eee " + e.getMessage());
             return Response.error(new ParseError(e));
         } catch (JSONException je) {
+            Log.d("htmlTAG", "jee " + je.getMessage());
             return Response.error(new ParseError(je));
         }
     }
