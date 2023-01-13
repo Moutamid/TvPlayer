9771dc65a298e54a7eb0be780dee6ab1b4b83816
suleman81 <sunnybajwa904@gmail.com>
2023-01-13 18:03:45 +0500

    commit

--- app/src/main/java/com/moutamid/tvplayer/MainActivity.java
+++ app/src/main/java/com/moutamid/tvplayer/MainActivity.java
@@ -176,8 +176,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             return  true;
         });
 
-
-
         return super.onCreateOptionsMenu(menu);
     }
 
